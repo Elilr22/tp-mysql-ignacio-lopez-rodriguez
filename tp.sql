f75ea71248ad35e3cc644c1ef5045841b70d8971
elilr22 <igju06@hotmail.com>
2025-12-16 00:54:19 -0300

    resolucion del tp

--- tp.sql
+++ tp.sql
@@ -1,3 +1,96 @@
 --Ejercicio 1 – Crear Base de Datos 
 --Crear una base de datos llamada veterinaria_patitas_felices.
 CREATE DATABASE veterinaria_patitas_felices;
+
+--Ejercicio 2 – Crear tabla duenos
+--Crear la tabla duenos con las siguientes columnas:
+
+CREATE TABLE duenos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+    direccion VARCHAR(100)
+)
+
+--Ejercicio 3 – Crear tabla mascotas
+--Crear la tabla mascotas con las siguientes columnas:
+CREATE TABLE mascotas(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    especie VARCHAR(30) NOT NULL,
+    fecha_nacimiento DATE,
+    id_dueno INT,
+    FOREIGN KEY (id_dueno) REFERENCES duenos(id)
+)
+
+--Ejercicio 4 – Crear tabla veterinarios
+--Crear la tabla veterinarios con las siguientes columnas:
+
+CREATE TABLE veterinarios(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    matricula VARCHAR(20) UNIQUE NOT NULL,
+    especialidad VARCHAR(50) NOT NULL
+)
+
+--Ejercicio 5 – Crear tabla historial_clinico
+--Crear la tabla historial_clinico con las siguientes columnas:
+
+CREATE TABLE historial_clinico(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_mascotas INT,
+    FOREIGN KEY (id_mascotas) REFERENCES mascotas(id),
+    id_veterinario INT,
+    FOREIGN KEY (id_veterinario) REFERENCES veterinarios(id),
+    fecha_registro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    descripcion VARCHAR(250) NOT NULL
+)
+
+--Ejercicio 6 – Insertar registros
+--3 dueños con información completa
+INSERT INTO duenos (nombre, apellido, telefono, direccion)
+VALUES
+('Ana', 'García', '1123456789','Avenida Rivadavia 5890 Caballito'),
+('Roberto', 'Martínez', '1198765432','Calle Juncal 1345	Recoleta'),
+('Sofía', 'López', '1150050050','Pasaje Lanín 321 La Boca');
+('Ignacio', 'López Rodriguez', '1161804354','Lobos 848');
+
+--3 mascotas, cada una asociada a un dueño
+INSERT INTO mascotas (nombre, especie, fecha_nacimiento, id_dueno)
+VALUES
+('Max', 'Perro', '2022-05-15', 1),
+('Luna', 'Gato', '2023-01-20', 2),
+('tyson','perro','2012-01-10',3);
+
+--2 veterinarios con especialidades distintas
+INSERT INTO veterinarios (nombre, apellido, matricula, especialidad)
+VALUES
+('Juan', 'Pablo', '20514236', 'Cirugía'),
+('Georgina', 'Del Barba', '20415162', 'Cardiología');
+
+--3 registros de historial clínico
+INSERT INTO historial_clinico (id_mascotas,id_veterinario,fecha_registro,descripcion)
+VALUES
+('1','1','2025-11-15','se ha recuperado bien de la cirugía. Le hicimos una laparotomía exploratoria porque tenía una obstrucción intestinal. Logramos identificar y retirar un juguete que estaba causando el bloqueo'),
+('2','2','2025-11-25','Se observa cardiomegalia global (tamaño cardíaco aumentado)')
+
+
+--Ejercicio 7 – Actualizar registros
+--1. Cambiar la dirección de un dueño (por ID o nombre).
+UPDATE duenos
+SET direccion = 'suipacha 1122'
+WHERE id = 1
+
+--2. Actualizar la especialidad de un veterinario (por ID o matrícula).
+UPDATE veterinarios
+SET especialidad = 'Cirugia Ortopedia'
+WHERE id = 1
+
+--3. Editar la descripción de un historial clínico (por ID).
+UPDATE historial_clinico
+SET descripcion = 'Se observa cardiomegalia global, La tráquea y bronquios principales se ven normales'
+WHERE id = 2
+
+
